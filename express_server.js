5693e5c11f3428432d72f161b647823feae67649
TheMartonfi <carlmartonfi@hotmail.com>
2020-08-31 22:05:19 +0000

    Added body-parser dependency and moved specific path higher so it would render properly

--- express_server.js
+++ express_server.js
@@ -1,8 +1,11 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+
 const app = express();
 const PORT = 8080;
 
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -18,11 +21,11 @@ app.get("/urls", (req, res) => {
   res.render('urls_index', templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render('urls_new');
+});
+
 app.get("/urls/:shortURL", (req, res) => {
   let templateVars = { shortURL: req.params.shortURL, longURL: req.params.longURL };
   res.render('urls_show', templateVars);
-});
-
-app.get("/urls/new", (req, res) => {
-  res.render('urls_new');
 });
