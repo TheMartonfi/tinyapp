4634841c3fc18bd374b40e793be3677aee7a1063
TheMartonfi <carlmartonfi@hotmail.com>
2020-08-31 21:18:59 +0000

    Added ejs for urls and added path for /urls

--- express_server.js
+++ express_server.js
@@ -13,15 +13,8 @@ app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
 
-app.get("/", (req, res) => {
-  res.send("Hello!");
+app.get("/urls", (req, res) => {
+  let templateVars = { urls: urlDatabase }
+  res.render('urls_index', templateVars);
 });
 
-
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
-});
-
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
